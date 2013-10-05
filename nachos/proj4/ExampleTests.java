08089909dc50dee4e444e86b0e49d8cb984a06ca
Kevin Klues <klueska@gmail.com>
2013-10-04 17:02:17 -0700

    Update ExampleTests to better reflect proper JUnit assertion pattern

--- nachos/proj4/ExampleTests.java
+++ nachos/proj4/ExampleTests.java
@@ -1,5 +1,7 @@
 package nachos.proj4;
 
+import nachos.threads.KThread;
+
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
@@ -7,31 +9,33 @@ public class ExampleTests {
 	
 	@Test
 	public void exampleTest1() {
+        System.out.println("Starting Project 4 exampleTest1!");
 		UnitTests.enqueueJob(new Runnable() {
 
 			@Override
 			public void run() {
-                System.out.println("Starting Project 4 exampleTest1!");
-				// Uncomment to see the unit test fail!
-				//assertTrue("Project 4 exampleTest1 failed!", false);
-                System.out.println("Finishing Project 4 exampleTest1!");
+				KThread.yield();
 			}
 
 		});
+		// Uncomment to see the unit test fail!
+		//assertTrue("Project 4 exampleTest1 failed!", false);		
+        System.out.println("Finishing Project 4 exampleTest1!");
 	}
 	
 	@Test
 	public void exampleTest2() {
+        System.out.println("Starting Project 4 exampleTest2!");
 		UnitTests.enqueueJob(new Runnable() {
 
 			@Override
 			public void run() {
-                System.out.println("Starting Project 4 exampleTest2!");
-				// Uncomment to see the unit test fail!
-				//assertTrue("Project 4 exampleTest2 failed!", false);
-                System.out.println("Finishing Project 4 exampleTest2!");
+				KThread.yield();
 			}
 
 		});
+		// Uncomment to see the unit test fail!
+		//assertTrue("Project 4 exampleTest1 failed!", false);		
+        System.out.println("Finishing Project 4 exampleTest2!");
 	}
 }
